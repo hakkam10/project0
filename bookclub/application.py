b0be4faaf953a285d7f2c3c775d8faae384d630b
hakkam10 <abdulhakkam23@gmail.com>
2020-06-18 21:47:52 +0530

    layout2 finished

--- bookclub/application.py
+++ bookclub/application.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+@app.route("/book")
+def book():
+    return render_template("book.html")
