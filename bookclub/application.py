99b6da1c2d2fe45438f50a41af8fb1d56b1b0cfe
hakkam10 <abdulhakkam23@gmail.com>
2020-06-20 16:54:32 +0530

    login successful

--- bookclub/application.py
+++ bookclub/application.py
@@ -1,15 +1,65 @@
-from flask import Flask, render_template, request
+import os
+
+from flask import Flask, render_template, request, session, url_for
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/search")
+@app.route("/registered", methods=["POST"])
+def registered():
+
+    """update user"""
+    # get user information
+    try:
+        name = request.form.get("name")
+        username = request.form.get("username")
+        password = request.form.get("password")
+    except ValueError:
+        return render_template("error.html", message="Invalid name or user name.")
+
+    #update users
+    db.execute("INSERT INTO users (name, username, password) VALUES (:name, :username, :password)",
+            {"name": name, "username": username, "password": password})
+    db.commit()
+    return render_template("registered.html")
+
+@app.route("/search", methods=["POST"])
 def search():
-    return render_template("search.html")
+    # get login information
+    username = request.form.get("username")
+    password = request.form.get("password")
+    user_id = db.execute("SELECT user_id FROM users WHERE (username = :username) AND (password = :password)",
+                        {"username": username, "password": password})
+    # invalid
+    if db.execute("SELECT user_id FROM users WHERE (username = :username) AND (password = :password)", {"username": username, "password": password}).rowcount == 0:
+        return render_template("error.html", message="Invalid username or password")
+    #valid    
+    if db.execute("SELECT user_id FROM users WHERE (username = :username) AND (password = :password)", {"username": username, "password": password}).rowcount == 1:
+        return render_template("search.html", name=user_id)
 
 @app.route("/book")
 def book():
     return render_template("book.html")
+
+@app.route("/result")
+def result():
+    return render_template("results.html")
